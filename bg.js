5a269065cf24b2e114a25e206b968fc1d7388020
Kevin Kim <kevink1103@gmail.com>
2019-02-26 22:13:57 +0800

    fix: image load optimization

--- bg.js
+++ bg.js
@@ -5,9 +5,10 @@ const IMG_NUMBER = 5
 function paintImage(imgNumber) {
     const image = new Image()
     image.src = `img/${imgNumber + 1}.jpg`
+    image.classList.add("bgImage")
     image.addEventListener("load", () => {
         console.log("Image Loaded")
-        image.classList.add("bgImage")
+        image.classList.add("animateBg")
     })
     body.prepend(image)
 }
